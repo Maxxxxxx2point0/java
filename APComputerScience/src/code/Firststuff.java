8c82882a7f7ec96b32ab2e75dc14813fbf91bfdf
Maxxxxxx2point0 <maxthecamaroboy.12@gmail.com>
2019-08-18 13:25:21 -0600

    First commit

---
+++ APComputerScience/src/code/Firststuff.java
@@ -0,0 +1,20 @@
+package code;
+
+/*public class hello { /*<--class header | { - curly braces
+/*main method | method header   public static void main(String[] args){
+  /* statement     System.out.print("Friday is here");
+}
+}
+
+
+/*
+class - method - actions/computations
+*/
+public class Firststuff {
+public static void main(String[] args){
+  for (int i = 0; i <=4; i++){
+    System.out.println("Line " + i);
+  }
+}
+}
+
